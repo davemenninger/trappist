6ca3801f3c19a839dd2eacef8f8c93c65843f481
Dave Menninger <dave.menninger@gmail.com>
2018-10-09 23:14:44 -0400

    add rudimentary hit detection

--- client/client.js
+++ client/client.js
@@ -28,7 +28,7 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     Client.socket.on('remove', function(id) {
-        Game.removePlayer(id);
+        game.removePlayer(id);
     });
 
     Client.sendClick = function(x, y) {
@@ -40,4 +40,35 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // go
     Client.askNewPlayer();
+
+    document.onreadystatechange = function() {
+        if (document.readyState == "complete") {
+
+            var canvas = document.getElementById('map1');
+            canvas.addEventListener('click', function(event) {
+                    console.log('click');
+                    var rect = canvas.getBoundingClientRect();
+                    var selected_pixel = {
+                        x: (event.clientX - rect.left) / (rect.right - rect.left) * canvas.width,
+                        y: (event.clientY - rect.top) / (rect.bottom - rect.top) * canvas.height
+                    };
+
+                    // for each planet, was this click within radius of the center of the planet
+                    for (var i = 0; i < game.planets.length; i++) {
+                        var p = game.planets[i];
+                        // a squared plus b squared equals c squared
+                        if (
+                            Math.pow(selected_pixel.x - p.x, 2) // x distance to center
+                            +
+                            Math.pow(selected_pixel.y - p.y, 2) // y distance to center
+                            <=
+                            Math.pow(p.radius * 9, 2) // arbitrary 9 matches one in draw function
+                        ) {
+                            console.log('hit ' + p.name);
+                        }
+                    }
+                },
+                false);
+        }
+    };
 });
